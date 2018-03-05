463ea3d71b74e75f7c4c70e99484616d3f8bb973
GodofMunch <david.osullivan5@students.ittralee.ie>
2018-03-05 23:33:25 +0000

    Caught up on Lab work on arrays

--- Lab2/src/MyArrays.java
+++ Lab2/src/MyArrays.java
@@ -1,5 +1,13 @@
 public class MyArrays {
-
+    /**
+     * Method = removeItem
+     * Description = remove an item from an integer array given the index of the item to be removed
+     * @author Dave O'Sullivan
+     * @param list
+     * @param currentSize
+     * @param position
+     * @return int
+     */
     public static int removeItem(int [] list, int currentSize, int position) {
 
         for(int i = position+1; i < currentSize; i ++) {
@@ -9,6 +17,18 @@ public class MyArrays {
 
         return currentSize-1;
     }
+
+    /**
+     * Method = addItem
+     * Description = add an item to an integer array given the index of where the item is to go and
+     *               given the item to be added
+     * @author Dave O'Sullivan
+     * @param list
+     * @param currentSize
+     * @param position
+     * @param newValue
+     * @return
+     */
 
     public static int addItem(int[] list, int currentSize, int position, int newValue)
     {
@@ -24,6 +44,14 @@ public class MyArrays {
         return currentSize;
     }
 
+    /**
+     * Method = sumArray
+     * Description = return the sum of values of an integer array
+     * @author Dave O'Sullivan
+     * @param list
+     * @param currentSize
+     * @return
+     */
     public static int sumArray(int[] list, int currentSize) {
 
         int sum = 0;
@@ -34,6 +62,14 @@ public class MyArrays {
         return sum;
     }
 
+    /**
+     * Method = maxOfArray
+     * Description = return the largest value of an integer array
+     * @author Dave O'Sullivan
+     * @param list
+     * @param currentSize
+     * @return
+     */
     public static int maxOfArray(int[] list, int currentSize) {
         int max = list[0];
 
@@ -44,6 +80,14 @@ public class MyArrays {
         return max;
     }
 
+    /**
+     * Method = minOfArray
+     * Description = return the smallest value of an integer array
+     * @author Dave O'Sullivan
+     * @param list
+     * @param currentSize
+     * @return
+     */
     public static int minOfArray(int[] list, int currentSize) {
         int min = list[0];
 
@@ -52,5 +96,83 @@ public class MyArrays {
                 min = list[i];
 
         return min;
+    }
+
+    /**
+     * Method = linearSearch
+     * Description = returns the index of an integer value in an array if it is found.
+     *               If it is not found, the method returns -1.
+     * @author Dave O'Sullivan
+     * @param a
+     * @param currentSize
+     * @param searchKey
+     * @return
+     */
+    public static int linearSearch(int[] a, int currentSize, int searchKey) {
+
+        int i = 0;
+        boolean found = false;
+
+        while(!found && i < currentSize) {
+            if(a[i] == searchKey)
+                found = true;
+            else
+                i++;
+        }
+
+        if(found)
+            return i;
+        else
+            return -1;
+    }
+
+    /**
+     * Method = merge
+     * Description = returns an integer array that is merged from two other sorted arrays
+     * @author Dave O'Sullivan
+     * @param aOne
+     * @param leftOne
+     * @param rightOne
+     * @param aTwo
+     * @param leftTwo
+     * @param rightTwo
+     * @return
+     */
+    public static int[] merge(int[] aOne, int leftOne, int rightOne, int[] aTwo, int leftTwo, int rightTwo) {
+
+        int i = leftOne;
+        int j = leftTwo;
+        int k = 0;
+
+        int[] aThree = new int[rightOne + rightTwo];
+
+        while (i <= rightOne && j <= rightTwo) {
+
+            if (aOne[i] <= aTwo[j]) {  //Crashing on i == 4 whereas aOnes's last index == 3
+
+                aThree[k] = aOne[i];
+                i++;
+                k++;
+            } else if (aOne[i] > aTwo[j]) {
+
+                aThree[k] = aTwo[j];
+                j++;
+                k++;
+            }
+        }
+
+        if (i <= rightOne)
+            for (int m = i; m < rightOne; m++) {
+                aThree[k] = aOne[m];
+                k++;
+            }
+
+        else
+            for (int m = j; m < rightTwo; m++) {
+                aThree[k] = aTwo[m];
+                k++;
+        }
+
+        return aThree;
     }
 }
--- Lab2/src/MyArraysTest.java
+++ Lab2/src/MyArraysTest.java
@@ -5,6 +5,10 @@ public class MyArraysTest {
         testRemoveItem();
         testAddItem();
         testSum();
+        testMin();
+        testMax();
+        testLinearSearch();
+        testMerge();
     }
 
     public static void testRemoveItem() {
@@ -30,7 +34,7 @@ public class MyArraysTest {
         for (int i = 0; i < listOfItems.length; i ++)
             arrayText += "\n" + listOfItems[i];
 
-        System.out.println(arrayText);
+        System.out.println("\n\nTest Remove Item\n\n" + arrayText);
         itemsUsed = MyArrays.removeItem(listOfItems, listOfItems.length, 3);
 
         arrayText = "";
@@ -39,7 +43,7 @@ public class MyArraysTest {
             arrayText += "\n" + listOfItems[i];
         }
 
-        System.out.print(arrayText);
+        System.out.print("\n\nExpected :\n12\n24\n36\n48\n0 \n\nActual :" + arrayText);
     }
 
     public  static  void testAddItem() {
@@ -48,11 +52,11 @@ public class MyArraysTest {
 
         int[] array = {2,4,6,8,10,12,16,18,20,0,0,0,0,0};
 
-        String arrayText = "\n\n";
+        String arrayText = "";
 
         for(int i = 0; i < array.length;i++)
         {
-            if(array[i] != 0 && array[i+1] ==0)
+            if(array[i] != 0 && array[i+1] == 0)
                 currentSize = i;
         }
 
@@ -61,7 +65,7 @@ public class MyArraysTest {
         for(int i = 0; i < array.length; i++)
             arrayText += array[i] + ",";
 
-        System.out.println(arrayText);
+        System.out.println("\n\nTest add Item\n\nExpected 2,4,6,8,10,12,14,16,18,20,22,0,0,0,0 \nActual = " + arrayText);
     }
 
     public static void testSum() {
@@ -69,5 +73,53 @@ public class MyArraysTest {
 
         int sum = MyArrays.sumArray(array,array.length);
 
+        System.out.print("\n\nTest Sum:\n\nExpected - 210 - Actual - " + sum);
+
+    }
+
+    public static void testMin() {
+        int[] list = {2,4,6,8,0,34,22,66};
+
+        int smallest = MyArrays.minOfArray(list, list.length);
+
+        System.out.println("\n\nTest Minimum of Array\nExpected : 0\nActual :  " + smallest);
+    }
+
+    public static void testMax() {
+        int[] list = {23, 67, 1, 67, 89, 45, 99, 12};
+
+        int largest = MyArrays.maxOfArray(list, list.length);
+
+        System.out.println("\n\nTest Maximum of array\nExpected : 99\nActual :  " + largest);
+    }
+
+    public static void testLinearSearch() {
+        int[] list = {12,23,34,45,56,67,78,89,90};
+
+        int key = 34;
+        int indexOfKey = MyArrays.linearSearch(list, list.length, key);
+
+        System.out.println("\n\nTest Linear Search\n\n Looking for " + key + "\nExpected : 2\nActual : " + indexOfKey +
+                            " (found at index " + indexOfKey + ")");
+
+        key = 44;
+        indexOfKey = MyArrays.linearSearch(list, list.length, key);
+
+        System.out.println("\n\nTest Linear Search\n\nLooking for " + key + "\nExpected : -1 (not found) " +
+                           "Actual : " + indexOfKey);
+    }
+
+    public static void testMerge() {
+
+        int[] sortedArrayOne = {23,55,66,99};
+        int[] sortedArrayTwo = {11,12,34,100,156,200};
+
+        int[] mergedArray = MyArrays.merge(sortedArrayOne, 0, sortedArrayOne.length, sortedArrayTwo, 0, sortedArrayTwo.length);
+
+        String mergedArrayToString = "\n";
+        for(int i = 0; i < mergedArray.length; i++)
+            mergedArrayToString += mergedArray[i] + ",";
+
+        System.out.println("\n\nTest Merge\n\nExpected 11,12,23,34,55,66,99,100,156,200,\nActual : " + mergedArrayToString);
     }
 }
